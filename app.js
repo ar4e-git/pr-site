a93f09ed13843d334cec88ddfecffa8d8968984d
Alek <ar4e-git@list.ru>
2024-02-29 16:56:21 +0400

    Changed url

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var player = new Playerjs({id: "player", file: "https://pr-prod.onrender.com/c115273b8b483e5375924ba490691e5a.m3u8", poster:"muztv-1.png"});
+const mtvaz = `mtvazerbaycan`;
+var player = new Playerjs({id: "player", file: `https://ex-my-prod.onrender.com/channel/${mtvaz}.m3u8`, poster:"muztv-1.png"});
 let time100;
 function PlayerjsEvents(event,player,info){
     if(x=="time"){
